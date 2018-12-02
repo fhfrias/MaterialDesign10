a9b125041c130ec6a4b39c428eeedf31b07c124e
Francisco Javier Hidalgo Frias <friasjavi45@gmail.com>
2018-12-02 21:37:06 +0100

    Primer paso MaterialDesign10

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign10/Material10.java
@@ -0,0 +1,13 @@
+package com.iesvirgendelcarmen.dam.materialdesign10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Material10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material10);
+    }
+}
